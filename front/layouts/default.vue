7f12f1a2424c3ba9ad6131d77ef44c16fe55d0f3
Lu Sacramento <lucianossacramento@yahoo.com.br>
2022-10-21 14:05:06 -0300

    front - configured basic layouts

---
+++ front/layouts/default.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app" class="app container-fluid">
+    <div id="nav">
+      <Nav :base-url="baseUrl" />
+    </div>
+    <main id="main" class="router-view">
+      <Nuxt />
+    </main>
+    <div id="footer">
+      <Footer />
+    </div>
+  </div>
+</template>
+
+<script>
+import Nav from '@/components/navAndFooter/Nav'
+import Footer from '@/components/navAndFooter/Footer'
+export default {
+  name: 'DefaultLayout',
+  components: { Footer, Nav },
+  data() {
+    return {
+      baseUrl: 'http://localhost:3000/',
+    }
+  },
+}
+</script>
+<style>
+@media (min-width: 991.98px) {
+  div.app {
+    grid-template-rows: 17vh 1fr 11vh;
+  }
+  /* .layer-top-exercise {
+      height: 42vh !important;
+    } */
+}
+div.app {
+  display: grid;
+
+  grid-template-areas:
+    'nav'
+    'main'
+    'foot';
+}
+#nav {
+  grid-area: nav;
+}
+#main {
+  grid-area: main;
+}
+#footer {
+  grid-area: foot;
+}
+</style>
---
+++ front/layouts/error.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 v-if="error.statusCode === 404">Página não encontrada</h1>
+    <h1 v-else>Ocorreu um erro</h1>
+    <NuxtLink to="/">Página Inicial</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ErrorPage',
+  layout: 'error', // você pode definir um layout personalizado para a página de erro
+  // props: [error]
+}
+</script>
