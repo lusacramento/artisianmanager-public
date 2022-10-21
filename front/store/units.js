f759134255268009394804f8a5c6e7f03e252e4a
Lu Sacramento <lucianossacramento@yahoo.com.br>
2022-10-21 14:11:07 -0300

    front - implemented Units store

---
+++ front/store/units.js
@@ -0,0 +1,76 @@
+// States
+export const state = () => ({
+  units: [],
+  unit: {
+    id: '',
+    name: '',
+    symbol: '',
+    registerDate: '',
+  },
+})
+
+// Mutations
+export const mutations = {
+  setUnits(state, value) {
+    state.units = value
+  },
+
+  setUnit(state, value) {
+    state.unit = value
+  },
+
+  setName(state, value) {
+    state.unit.name = value
+  },
+
+  setSymbol(state, value) {
+    state.unit.symbol = value
+  },
+
+  deleteUnit(state, position) {
+    state.units.splice(position, 1)
+  },
+}
+
+// Actions
+export const actions = {
+  // Database Operations
+  async getUnits({ state, commit }) {
+    const response = await this.$axios.$get('http://localhost:8080/api/units/')
+    commit('setUnits', response)
+  },
+
+  async postUnit({ state, commit }) {
+    // Validating data
+    if (state.unit.name !== '' && state.unit.symbol !== '')
+      await this.$axios.$post(
+        'http://localhost:8080/api/units/insert',
+        state.unit
+      )
+  },
+
+  async deleteUnit({ state, commit }, data) {
+    const isDeleted = await this.$axios.$delete(
+      `http://localhost:8080/api/units/delete?_id=${data.unit._id}`
+    )
+    isDeleted
+      ? commit('deleteUnit', data.position)
+      : alert('Erro ao apagar dado')
+  },
+
+  // Store operations
+  setUnits({ state, commit }, value) {
+    commit('setUnits', value)
+  },
+
+  setUnit({ state, commit }, value) {
+    commit('setUnit', value)
+  },
+
+  setName({ state, commit }, value) {
+    commit('setName', value)
+  },
+  setSymbol({ state, commit }, value) {
+    commit('setSymbol', value)
+  },
+}
