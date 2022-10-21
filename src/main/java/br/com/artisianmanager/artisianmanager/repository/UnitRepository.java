721c54213ba9716c91de51acd867d4509ecd631e
Lu Sacramento <lucianossacramento@yahoo.com.br>
2022-10-21 13:44:14 -0300

    implemented Unit repository

---
+++ src/main/java/br/com/artisianmanager/artisianmanager/repository/UnitRepository.java
@@ -0,0 +1,9 @@
+package br.com.artisianmanager.artisianmanager.repository;
+
+import br.com.artisianmanager.artisianmanager.model.entity.Unit;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UnitRepository extends MongoRepository<Unit, String> {
+}
