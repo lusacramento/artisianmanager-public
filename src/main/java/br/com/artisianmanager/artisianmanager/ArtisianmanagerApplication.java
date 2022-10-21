5891506e7a6d77210087fe3a1f83a08bde144caa
Lu Sacramento <lucianossacramento@yahoo.com.br>
2022-10-21 13:37:42 -0300

    add main spring aplication

---
+++ src/main/java/br/com/artisianmanager/artisianmanager/ArtisianmanagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.artisianmanager.artisianmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtisianmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtisianmanagerApplication.class, args);
+	}
+
+}
