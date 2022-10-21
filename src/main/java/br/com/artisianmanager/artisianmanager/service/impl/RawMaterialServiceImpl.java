83caa471aea2b4173ce982f3db7870366c2e099e
Lu Sacramento <lucianossacramento@yahoo.com.br>
2022-10-21 13:48:52 -0300

    implemented Raw Material service

---
+++ src/main/java/br/com/artisianmanager/artisianmanager/service/impl/RawMaterialServiceImpl.java
@@ -0,0 +1,45 @@
+package br.com.artisianmanager.artisianmanager.service.impl;
+
+import br.com.artisianmanager.artisianmanager.model.entity.RawMaterial;
+import br.com.artisianmanager.artisianmanager.repository.RawMaterialRepository;
+import br.com.artisianmanager.artisianmanager.service.RawMaterialService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RawMaterialServiceImpl implements RawMaterialService {
+
+    @Autowired
+    private RawMaterialRepository rawMaterialRepository;
+
+    @Override
+    public List<RawMaterial> findAll() {
+
+        return rawMaterialRepository.findAll();
+    }
+
+    @Override
+    public Optional<RawMaterial> findById(String _id){
+
+        return rawMaterialRepository.findById(_id);
+    }
+
+    @Override
+    public RawMaterial save(RawMaterial rawmaterial) {
+        return rawMaterialRepository.save(rawmaterial);
+    }
+
+    @Override
+    public boolean deleteById(String _id) {
+        try {
+            rawMaterialRepository.deleteById(_id);
+            return true;
+        } catch (Exception e){
+            new IllegalArgumentException("Não encontrado!");
+            return false;
+        }
+    }
+}
