02de26702bb5f125432e9bf7f16cd1f5c5c4c2e3
Lu Sacramento <lucianossacramento@yahoo.com.br>
2022-10-29 18:19:36 -0300

    implemented unit entity test

---
+++ src/test/java/br/com/artisianmanager/artisianmanager/model/entity/UnitTest.java
@@ -0,0 +1,43 @@
+package br.com.artisianmanager.artisianmanager.model.entity;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+
+public class UnitTest {
+    @Autowired
+    public Unit unit = new Unit();
+    public LocalDateTime dateTime = LocalDateTime.now();
+
+    @BeforeEach
+    void setUp() {
+        unit = new Unit();
+    }
+
+    @Test
+    void unitSetters(){
+    Unit unit = setUnit();
+    };
+
+    @Test
+    void getUnit(){
+        setUnit();
+        Assertions.assertEquals("111", this.unit.get_id());
+        Assertions.assertEquals("Quilo", this.unit.getName());
+        Assertions.assertEquals("Kg", this.unit.getSymbol());
+        Assertions.assertEquals(dateTime, this.unit.getRegisterDate());
+        Assertions.assertEquals(dateTime, this.unit.getUpdateDate());
+    }
+
+    public Unit setUnit(){
+        this.unit.set_id("111");
+        this.unit.setName("Quilo");
+        this.unit.setSymbol("Kg");
+        this.unit.setRegisterDate();
+        this.unit.setUpdateDate();
+        return this.unit;
+    }
+}
