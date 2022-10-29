69deca3e8fbcba82074e557eba79328b53985a44
Lu Sacramento <lucianossacramento@yahoo.com.br>
2022-10-29 18:21:28 -0300

    refactoring Unit controller for filds validations

--- src/main/java/br/com/artisianmanager/artisianmanager/controller/UnitController.java
+++ src/main/java/br/com/artisianmanager/artisianmanager/controller/UnitController.java
@@ -2,6 +2,7 @@ package br.com.artisianmanager.artisianmanager.controller;
 
 import br.com.artisianmanager.artisianmanager.model.entity.Unit;
 import br.com.artisianmanager.artisianmanager.service.UnitService;
+import br.com.artisianmanager.artisianmanager.service.UtilsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +14,10 @@ public class UnitController {
 
     @Autowired
     private UnitService unitService;
+
+    @Autowired
+    private UtilsService utilsService;
+
      @GetMapping("api/units")
      public List<Unit> findAll(){
         return this.unitService.findAll();
@@ -20,7 +25,29 @@ public class UnitController {
 
     @PostMapping("api/units/insert")
     public Unit save(@RequestBody Unit unit){
-         return this.unitService.save(unit);
+         if(this.utilsService.verifyNullField(unit.getName()) || this.utilsService.verifyNullField(unit.getSymbol())){
+             return null;
+         }
+         else {
+
+             // verify if exists id
+             boolean isExistUnit = false;
+             if (unit.get_id() != null) {
+                 isExistUnit = this.unitService.existsById(unit.get_id());
+             }
+
+             // Case exist the id, don't set new date in the register date
+             if (isExistUnit) {
+                 unit.setRegisterDate(this.unitService.findById(unit.get_id()).orElse(null).getRegisterDate());
+
+             } else {
+                 unit.setRegisterDate(this.utilsService.getDate());
+             }
+
+             unit.setUpdateDate(this.utilsService.getDate());
+
+             return this.unitService.save(unit);
+         }
     }
 
     @DeleteMapping("api/units/delete")
