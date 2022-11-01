d8e64263e5ac46feff20c3ef57722a20e81e2dce
Lu Sacramento <lucianossacramento@yahoo.com.br>
2022-11-01 18:16:26 -0300

    configuring Unity controller test

--- src/test/java/br/com/artisianmanager/artisianmanager/controller/UnitControllerTest.java
+++ src/test/java/br/com/artisianmanager/artisianmanager/controller/UnitControllerTest.java
@@ -1,25 +1,39 @@
 package br.com.artisianmanager.artisianmanager.controller;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import br.com.artisianmanager.artisianmanager.model.entity.Unit;
+import br.com.artisianmanager.artisianmanager.model.entity.UnitTest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @AutoConfigureMockMvc
 class UnitControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    UnitController unitController;
+
+    UnitTest unit;
+    @BeforeEach
+    void setUp() {
+        this.unit = new UnitTest();
+    }
+
+
     @Test
     void findAll() throws Exception {
         mockMvc.perform(get("/api/units"))
@@ -46,6 +60,10 @@ class UnitControllerTest {
     }
 
     @Test
-    void deleteById() {
+    void deleteById() throws Exception{
+        String id = "111";
+        mockMvc.perform(delete("/api/units/" + id).contentType("application/json")
+                        .accept("application/json"))
+                .andExpect(status().isOk());
     }
 }
