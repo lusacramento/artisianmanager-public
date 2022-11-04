26be11efb4096db35883f78e0901beb1ec1f6b80
Lu Sacramento <lucianossacramento@yahoo.com.br>
2022-11-04 20:23:20 -0300

    implemented query find unit first by name.

--- src/main/java/br/com/artisianmanager/artisianmanager/repository/UnitRepository.java
+++ src/main/java/br/com/artisianmanager/artisianmanager/repository/UnitRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Service;
 @Service
 public interface UnitRepository extends MongoRepository<Unit, String> {
 
-    Unit queryFirstByName(String name, Unit unit);
+    boolean queryFirstByName(String name);
 }
