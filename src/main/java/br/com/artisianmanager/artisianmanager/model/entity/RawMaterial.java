03f5d6fcefdfbbdface496daaa2bccfcf851664b
Lu Sacramento <lucianossacramento@yahoo.com.br>
2022-11-04 16:04:59 -0300

    add getters and setters Lombok

--- src/main/java/br/com/artisianmanager/artisianmanager/model/entity/RawMaterial.java
+++ src/main/java/br/com/artisianmanager/artisianmanager/model/entity/RawMaterial.java
@@ -1,11 +1,14 @@
 package br.com.artisianmanager.artisianmanager.model.entity;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Getter
+@Setter
 @Document
 public class RawMaterial {
-
         @Id
         private String _id;
         private String name;
@@ -13,42 +16,4 @@ public class RawMaterial {
         private String unit;
         private String registerDate;
         private String updateDate;
-
-        // Getters ans Setters
-        public String get_id() {
-                return _id;
-        }
-        public void set_id(String _id) {
-                this._id = _id;
-        }
-        public String getName() {
-                return name;
-        }
-        public void setName(String name) {
-                this.name = name;
-        }
-        public String getDescription() {
-                return description;
-        }
-        public void setDescription(String description) {
-                this.description = description;
-        }
-        public String getUnit() {
-                return unit;
-        }
-        public void setUnit(String unit) {
-                this.unit = unit;
-        }
-        public String getRegisterDate() {
-                return registerDate;
-        }
-        public void setRegisterDate(String registerDate) {
-                this.registerDate = registerDate;
-        }
-        public String getUpdateDate() {
-                return updateDate;
-        }
-        public void setUpdateDate(String updateDate) {
-                this.updateDate = updateDate;
-        }
 }
--- src/main/java/br/com/artisianmanager/artisianmanager/model/entity/Recipe.java
+++ src/main/java/br/com/artisianmanager/artisianmanager/model/entity/Recipe.java
@@ -1,11 +1,15 @@
 package br.com.artisianmanager.artisianmanager.model.entity;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.ArrayList;
 
 @Document
+@Getter
+@Setter
 public class Recipe {
     @Id
     private String _id;
@@ -18,60 +22,4 @@ public class Recipe {
     private String registerDate;
     private String updateDate;
     private ArrayList<Ingredient> ingredients;
-
-    // Getters ans Setters
-    public String get_id() {
-        return _id;
-    }
-    public void set_id(String _id) {
-        this._id = _id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getInstructions() {
-        return instructions;
-    }
-    public void setInstructions(String instructions) {
-        this.instructions = instructions;
-    }
-    public int getPortions() {
-        return portions;
-    }
-    public void setPortions(int portions) {
-        this.portions = portions;
-    }
-    public String getUnit() {
-        return unit;
-    }
-    public void setUnit(String unit) {
-        this.unit = unit;
-    }
-    public int getTime() {
-        return time;
-    }
-    public void setTime(int time) {
-        this.time = time;
-    }
-    public ArrayList<Ingredient> getIngredients() {
-        return ingredients;
-    }
-    public void setIngredients(ArrayList<Ingredient> ingredients) {
-        this.ingredients = ingredients;
-    }
-    public String getRegisterDate() {
-        return registerDate;
-    }
-    public void setRegisterDate(String registerDate) {
-        this.registerDate = registerDate;
-    }
-    public String getUpdateDate() {
-        return updateDate;
-    }
-    public void setUpdateDate(String updateDate) {
-        this.updateDate = updateDate;
-    }
 }
