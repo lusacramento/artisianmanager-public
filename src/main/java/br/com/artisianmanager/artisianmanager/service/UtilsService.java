b56a20fa94647ed88d71f47fc08016ffd10ab895
Lu Sacramento <lucianossacramento@yahoo.com.br>
2022-11-08 15:20:27 -0300

    Refatored LocalDateTime to OffsetDateTime types of Utils service.

--- src/main/java/br/com/artisianmanager/artisianmanager/service/UtilsService.java
+++ src/main/java/br/com/artisianmanager/artisianmanager/service/UtilsService.java
@@ -2,15 +2,9 @@ package br.com.artisianmanager.artisianmanager.service;
 
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-
 @Service
 public interface UtilsService {
 
-    public String formatDate(LocalDateTime date);
-
-    String getDate();
-
-    boolean verifyNullField(String string);
+    String getNewDate();
 }
 
--- src/main/java/br/com/artisianmanager/artisianmanager/service/impl/UtilsServiceImpl.java
+++ src/main/java/br/com/artisianmanager/artisianmanager/service/impl/UtilsServiceImpl.java
@@ -3,8 +3,7 @@ package br.com.artisianmanager.artisianmanager.service.impl;
 import br.com.artisianmanager.artisianmanager.service.UtilsService;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.time.OffsetDateTime;
 
 @Service
 public class UtilsServiceImpl implements UtilsService {
@@ -12,21 +11,7 @@ public class UtilsServiceImpl implements UtilsService {
     String datePattern = "dd-MM-yyyy HH:mm:ss";
 
     @Override
-    public String formatDate(LocalDateTime date) {
-        return date.format(DateTimeFormatter.ofPattern(datePattern)).toString();
-    }
-
-    @Override
-    public String getDate() {
-        LocalDateTime date =  LocalDateTime.now();
-        String dateString = formatDate(date);
-        return dateString;
-    }
-
-    @Override
-    public boolean verifyNullField(String string) {
-        if(string == null || string == "")
-            return true;
-        return false;
+    public String getNewDate() {
+        return OffsetDateTime.now().toString();
     }
 }
