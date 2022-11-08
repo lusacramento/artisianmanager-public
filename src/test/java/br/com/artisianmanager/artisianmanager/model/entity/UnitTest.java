311e1e7dac4b407e34eda19e469ed2cb1e915880
Lu Sacramento <lucianossacramento@yahoo.com.br>
2022-11-08 17:48:18 -0300

    fix tests Unit entity

--- src/test/java/br/com/artisianmanager/artisianmanager/model/entity/UnitTest.java
+++ src/test/java/br/com/artisianmanager/artisianmanager/model/entity/UnitTest.java
@@ -1,22 +1,25 @@
 package br.com.artisianmanager.artisianmanager.model.entity;
 
+import br.com.artisianmanager.artisianmanager.service.UtilsService;
+import br.com.artisianmanager.artisianmanager.service.impl.UtilsServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 public class UnitTest {
-    @Autowired
     public Unit unit = new Unit();
-    public LocalDateTime dateTime = LocalDateTime.now();
+
+    @Autowired
+    private UtilsService utilsService;
 
     @BeforeEach
     void setUp() {
         unit = new Unit();
+        this.utilsService = new UtilsServiceImpl();
     }
-
     @Test
     void unitSetters(){
     Unit unit = setUnit();
@@ -25,19 +28,22 @@ public class UnitTest {
     @Test
     void getUnit(){
         setUnit();
+
+        String date = utilsService.getNewDate();
+        this.unit.setRegisterDate(date);
+        this.unit.setUpdateDate(date);
+
         Assertions.assertEquals("111", this.unit.get_id());
         Assertions.assertEquals("Quilo", this.unit.getName());
         Assertions.assertEquals("Kg", this.unit.getSymbol());
-//        Assertions.assertEquals(dateTime, this.unit.getRegisterDate());
-//        Assertions.assertEquals(dateTime, this.unit.getUpdateDate());
+        Assertions.assertEquals(date, this.unit.getRegisterDate());
+        Assertions.assertEquals(date, this.unit.getUpdateDate());
     }
 
     public Unit setUnit(){
         this.unit.set_id("111");
         this.unit.setName("Quilo");
         this.unit.setSymbol("Kg");
-//        this.unit.setRegisterDate();
-//        this.unit.setUpdateDate();
         return this.unit;
     }
 }
