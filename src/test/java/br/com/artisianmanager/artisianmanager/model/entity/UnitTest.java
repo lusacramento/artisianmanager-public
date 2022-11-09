347dfeba4608d4186cb91a0b49076dea7c8ef7f5
Lu Sacramento <lucianossacramento@yahoo.com.br>
2022-11-08 21:57:21 -0300

    clean code unit test

--- src/test/java/br/com/artisianmanager/artisianmanager/model/entity/UnitTest.java
+++ src/test/java/br/com/artisianmanager/artisianmanager/model/entity/UnitTest.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.time.OffsetDateTime;
 
 public class UnitTest {
-    public Unit unit = new Unit();
+    public Unit unit;
 
     @Autowired
     private UtilsService utilsService;
