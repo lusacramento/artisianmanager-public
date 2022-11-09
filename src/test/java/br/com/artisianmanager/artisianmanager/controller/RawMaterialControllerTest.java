0a3f5e4b7cd3b700b803b87740ea1df580dd2aaa
Lu Sacramento <lucianossacramento@yahoo.com.br>
2022-11-08 22:03:17 -0300

    implemented tests in Raw Material entity and controller

--- src/test/java/br/com/artisianmanager/artisianmanager/controller/RawMaterialControllerTest.java
+++ src/test/java/br/com/artisianmanager/artisianmanager/controller/RawMaterialControllerTest.java
@@ -12,8 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -28,36 +27,49 @@ class RawMaterialControllerTest {
 
     RawMaterialTest rawMaterialTest;
 
+    RawMaterial rawMaterial;
+
     @BeforeEach
     void setUp() {
         rawMaterialTest = new RawMaterialTest();
     }
 
     @Test
-    void findAll() throws Exception{
-        mockMvc.perform(get("/api/raw-materials"))
-                .andExpect(status().isOk());
+    void Crud() throws Exception {
+        save();
+        findAll();
+        findById();
+        deleteById();
+    }
+    void save()  throws Exception{
+        setUp();
+        this.rawMaterial = rawMaterialTest.setRawMaterial();
 
+        mockMvc.perform(post("/api/raw-materials")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(this.rawMaterial)))
+                .andExpect(status().isCreated());
     }
 
-    @Test
-    void findById() throws Exception{
-        String id = "111";
+    void findAll() throws Exception {
+        mockMvc.perform(get("/api/raw-materials"))
+                .andExpect(status().isOk());
+    }
+
+    void findById() throws Exception {
+        this.rawMaterial = rawMaterialTest.setRawMaterial();
+        String id = this.rawMaterial.get_id();
+
         mockMvc.perform(get("/api/raw-materials/" + id))
                 .andExpect(status().isOk());
     }
 
-    @Test
-    void save() throws Exception {
-        setUp();
-        RawMaterial rawMaterial = rawMaterialTest.setRawMaterial();
-
-        mockMvc.perform(post("/api/raw-materials")
-                .contentType("application/json")
-                .content(objectMapper.writeValueAsString(rawMaterial))).andExpect(status().isOk());
+    void deleteById() throws Exception{
+        this.rawMaterial = rawMaterialTest.setRawMaterial();
+        String id = this.rawMaterial.get_id();
+        mockMvc.perform(delete("/api/raw-materials/" + id).contentType("application/json")
+                        .accept("application/json"))
+                .andExpect(status().isNoContent());
     }
 
-    @Test
-    void deleteById() {
-    }
 }
--- src/test/java/br/com/artisianmanager/artisianmanager/model/entity/RawMaterialTest.java
+++ src/test/java/br/com/artisianmanager/artisianmanager/model/entity/RawMaterialTest.java
@@ -2,17 +2,13 @@ package br.com.artisianmanager.artisianmanager.model.entity;
 
 import br.com.artisianmanager.artisianmanager.service.UtilsService;
 import br.com.artisianmanager.artisianmanager.service.impl.UtilsServiceImpl;
-import lombok.Setter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 public class RawMaterialTest {
 
-    @Autowired
     RawMaterial rawMaterial;
 
     @Autowired
@@ -20,30 +16,36 @@ public class RawMaterialTest {
 
     @BeforeEach
     void setUp() {
-        utilsService = new UtilsServiceImpl();
         rawMaterial = new RawMaterial();
+        utilsService = new UtilsServiceImpl();
     }
-
     @Test
-    void Getters(){
-        Setters();
-        Assertions.assertEquals("Material1", this.rawMaterial.getName(), "Material1");
-        Assertions.assertEquals("description material 1", this.rawMaterial.getDescription());
-        Assertions.assertEquals("Kg", this.rawMaterial.getUnit());
-    }
-
-    @Test
-    void Setters(){
+    void rawMaterialSetters(){
         setRawMaterial();
     }
 
+    @Test
+    void getRawMaterial(){
+        setRawMaterial();
+
+        String date = utilsService.getNewDate();
+        this.rawMaterial.setRegisterDate(date);
+        this.rawMaterial.setUpdateDate(date);
+
+        Assertions.assertEquals("Ovo", this.rawMaterial.getName(), "Material1");
+        Assertions.assertEquals("Ovo orgânico grande", this.rawMaterial.getDescription());
+        Assertions.assertEquals("und", this.rawMaterial.getUnit());
+        Assertions.assertEquals(date, this.rawMaterial.getRegisterDate());
+        Assertions.assertEquals(date, this.rawMaterial.getUpdateDate());
+    }
+
+
     public RawMaterial setRawMaterial(){
         setUp();
-        this.rawMaterial.setName("Material1");
-        this.rawMaterial.setDescription("description material 1");
-        this.rawMaterial.setUnit("Kg");
-        this.rawMaterial.setRegisterDate(this.utilsService.getDate());
-        this.rawMaterial.setUpdateDate(this.utilsService.getDate());
+        this.rawMaterial.set_id("222");
+        this.rawMaterial.setName("Ovo");
+        this.rawMaterial.setDescription("Ovo orgânico grande");
+        this.rawMaterial.setUnit("und");
         return this.rawMaterial;
 
     }
