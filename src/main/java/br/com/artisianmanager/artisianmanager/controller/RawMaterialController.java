4cd03e948925688d9234a9b4d77f2ada0ee647fc
Lu Sacramento <lucianossacramento@yahoo.com.br>
2022-11-08 22:02:41 -0300

    implemented functions in Raw Material controller

--- src/main/java/br/com/artisianmanager/artisianmanager/controller/RawMaterialController.java
+++ src/main/java/br/com/artisianmanager/artisianmanager/controller/RawMaterialController.java
@@ -1,16 +1,20 @@
 package br.com.artisianmanager.artisianmanager.controller;
 
+import br.com.artisianmanager.artisianmanager.domain.exception.NotFoundException;
 import br.com.artisianmanager.artisianmanager.model.entity.RawMaterial;
 import br.com.artisianmanager.artisianmanager.service.RawMaterialService;
 import br.com.artisianmanager.artisianmanager.service.UtilsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
-import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:8080")
 @RestController
+@RequestMapping("/api/raw-materials")
 public class RawMaterialController {
     @Autowired
     private RawMaterialService rawMaterialService;
@@ -18,48 +22,65 @@ public class RawMaterialController {
     @Autowired
     private UtilsService utilsService;
 
-    @GetMapping("api/raw-materials")
+    @GetMapping
     public List<RawMaterial> findAll(){
         return this.rawMaterialService.findAll();
     }
 
-    @RequestMapping(value = "/api/raw-materials/{_id}", method = RequestMethod.GET)
-    public Optional<RawMaterial> findById(@PathVariable("_id")String _id){
-        return this.rawMaterialService.findById(_id);
+    @GetMapping(value = "/{_id}")
+    public ResponseEntity<RawMaterial> findById(@PathVariable("_id")String _id){
+        return this.rawMaterialService.findById(_id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
-    @RequestMapping(value = "/api/raw-materials", method = RequestMethod.POST)
-    public RawMaterial save(@RequestBody RawMaterial rawMaterial){
-        //Validations
-        if(
-                this.utilsService.verifyNullField(rawMaterial.getName()) ||
-                this.utilsService.verifyNullField(rawMaterial.getDescription()) ||
-                this.utilsService.verifyNullField(rawMaterial.getUnit())
-        ){
-            return null;
-        } else {
-            // verify if exists id
-            boolean isExistRawMaterial = false;
-            if(rawMaterial.get_id() != null) {
-                isExistRawMaterial = this.rawMaterialService.existsById(rawMaterial.get_id());
-            }
-
-            // Case exist the id, don't set new date in the register date
-            if(isExistRawMaterial){
-                rawMaterial.setRegisterDate(this.rawMaterialService.findById(rawMaterial.get_id()).orElse(null).getRegisterDate());
-            } else {
-                rawMaterial.setRegisterDate(this.utilsService.getDate());
-            }
-
-            rawMaterial.setUpdateDate(this.utilsService.getDate());
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public RawMaterial save(@RequestBody @Valid RawMaterial rawMaterial){
             return this.rawMaterialService.save(rawMaterial);
-        }
     }
 
-    @DeleteMapping("api/raw-materials/{_id}")
-    @ResponseBody
-    public boolean deleteById(@PathVariable String _id){
-        return this.rawMaterialService.deleteById(_id);
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/add-all/{token}")
+    public List<RawMaterial> saveMany(@PathVariable String token, @Valid @RequestBody List<RawMaterial> rawMaterials){
+        String validPwd = "123";
+        if(token.equals(validPwd)){
+            return this.rawMaterialService.saveMany(rawMaterials);
+        }
+        return null;
+    }
+
+    @PutMapping("/{_id}")
+    public ResponseEntity<RawMaterial> alter(@Valid @PathVariable String _id, @RequestBody @Valid RawMaterial rawMaterial){
+        if(!rawMaterialService.existsById(_id)){
+            throw new NotFoundException("Dado não encontrado");
+        }
+
+        rawMaterial.set_id(_id);
+        rawMaterialService.alter(rawMaterial);
+        return ResponseEntity.ok(rawMaterial);
+    }
+
+    @DeleteMapping("/{_id}")
+    public ResponseEntity<Void> deleteById(@PathVariable String _id){
+        if(!rawMaterialService.existsById(_id)){
+            throw new NotFoundException("Matéria prima não encontrada! Contacte o suporte");
+        }
+
+        rawMaterialService.deleteById(_id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/delete-all/{token}")
+    public ResponseEntity<Void> deleteAll(@PathVariable String token){
+        String validPwd = "123";
+        if(token.equals(validPwd)){
+            this.rawMaterialService.deleteAll();
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
 
