daf0331599e755ed5d8e46e4f3ebbbffc6d3cb3b
Lu Sacramento <lucianossacramento@yahoo.com.br>
2022-11-09 13:37:58 -0300

    implemented query find by name method in Recipe repository.

--- src/main/java/br/com/artisianmanager/artisianmanager/repository/RecipeRepository.java
+++ src/main/java/br/com/artisianmanager/artisianmanager/repository/RecipeRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Service;
 
 @Service
 public interface RecipeRepository extends MongoRepository<Recipe, String> {
+    Recipe queryFirstByName(String name, Recipe recipe);
 }
