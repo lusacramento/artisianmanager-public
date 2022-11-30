d6f2b24c3269ea74c25184b721d835b062641c9b
Luciano Sacramento <lusacramento@pop-os.localdomain>
2022-11-30 17:43:39 -0300

    redesign bootstrap in menu

--- front/components/navAndFooter/Nav.vue
+++ front/components/navAndFooter/Nav.vue
@@ -1,37 +1,107 @@
 <template>
-  <div class="container-fluid">
-    <div class="row align-items-center">
-      <div class="col-12 col-lg-2">
-        <div class="text-center">
-          <li><nuxt-link to="/">Página Inicial</nuxt-link></li>
+  <div>
+    <nav class="navbar navbar-expand-lg">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Artisian Manager</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div id="navbarSupportedContent" class="collapse navbar-collapse">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Cadastro
+              </a>
+              <ul class="dropdown-menu">
+                <li>
+                  <nuxt-link class="dropdown-item" to="/units"
+                    >Unidade</nuxt-link
+                  >
+                </li>
+                <li>
+                  <nuxt-link class="dropdown-item" to="/raw-materials"
+                    >Matéria Prima</nuxt-link
+                  >
+                </li>
+                <li>
+                  <nuxt-link class="dropdown-item" to="/recipes"
+                    >Material Acabado</nuxt-link
+                  >
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Estoque
+              </a>
+              <ul class="dropdown-menu">
+                <li>
+                  <nuxt-link class="dropdown-item" to="#"
+                    >Matéria Prima</nuxt-link
+                  >
+                </li>
+                <li>
+                  <nuxt-link class="dropdown-item" to="#"
+                    >Material Acabado</nuxt-link
+                  >
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Relatórios
+              </a>
+              <ul class="dropdown-menu">
+                <li>
+                  <nuxt-link class="dropdown-item" to="/units"
+                    >Unidade</nuxt-link
+                  >
+                </li>
+                <li>
+                  <nuxt-link class="dropdown-item" to="/raw-materials"
+                    >Matéria Prima</nuxt-link
+                  >
+                </li>
+                <li>
+                  <nuxt-link class="dropdown-item" to="/recipes"
+                    >Material Acabado</nuxt-link
+                  >
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Sobre</a>
+            </li>
+          </ul>
         </div>
       </div>
-      <div class="col-12 col-lg-4">
-        <div class="text-center">Cadastro</div>
-        <ul class="d-flex justify-content-around">
-          <li><nuxt-link to="/units">Unidades</nuxt-link></li>
-          <li>
-            <nuxt-link to="/raw-materials">Matérias Primas</nuxt-link>
-          </li>
-          <li><nuxt-link to="/recipes">Materiais Acabados</nuxt-link></li>
-        </ul>
-      </div>
-      <div class="col-12 col-lg-3">
-        <div class="text-center">Entrada</div>
-        <ul class="d-flex justify-content-around">
-          <li>
-            <nuxt-link to="/raw-materials-inventory">Matérias Primas</nuxt-link>
-          </li>
-          <li><nuxt-link to="/#">Materiais Acabados</nuxt-link></li>
-        </ul>
-      </div>
-      <div class="col-12 col-lg-3">
-        <div class="text-center">Saida</div>
-        <ul class="d-flex justify-content-around">
-          <li><nuxt-link to="#">Produto Acabado</nuxt-link></li>
-        </ul>
-      </div>
-    </div>
+    </nav>
   </div>
 </template>
 
@@ -42,11 +112,32 @@ export default {
 </script>
 
 <style>
-ul {
-  display: flex;
-}
 li {
-  margin-inline: 10px;
-  list-style: none;
+  padding: 0 20px;
+}
+.navbar {
+  background-color: transparent;
+}
+.navbar-brand {
+  color: var(--secondary-color-100);
+  font-size: 1.5rem;
+}
+
+.dropdown > a,
+.nav-item > a {
+  color: var(--white-color-100);
+}
+
+.dropdown-menu {
+  background-color: var(--background-color);
+}
+
+.dropdown-item {
+  color: var(--white-color-80);
+}
+
+.dropdown-item:hover {
+  background-color: var(--background-color);
+  color: var(--white-color-100);
 }
 </style>
